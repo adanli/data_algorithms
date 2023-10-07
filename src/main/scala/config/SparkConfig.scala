0060ba3c7a39a9a5457321e1cf1d89b278fc9d4f
li adan <adanli@126.com>
2023-10-07 10:21:42 +0800

    feat(数据算法): init

--- src/main/scala/config/SparkConfig.scala
+++ src/main/scala/config/SparkConfig.scala
@@ -15,24 +15,25 @@ object SparkConfig {
   def config(): SparkConf = {
     new SparkConf()
       .setAppName("data_algorithms")
-      .setMaster("spark://11.0.0.193:7077")
+      .setMaster(if(isMac) "spark://localhost:7077" else "spark://11.0.0.193:7077")
       .set("spark.executor.memory", "4g")
       .set("spark.executor.cores", "1")
       .set("HADOOP_USER_NAME", "root")
-      .set("spark.driver.host", "11.0.0.199")
+      .set("spark.driver.host", if(isMac) "localhost" else "11.0.0.199")
       .set("spark.hadoop.fs.s3a.endpoint", "http://11.0.0.197:9000")
-      .set("spark.hadoop.fs.s3a.access.key", "zb38TwMyPsi3aGigkcfT")
-      .set("spark.hadoop.fs.s3a.secret.key", "ek7RlhyzafYLkb1YPIEgmfNRxIlF5FqHxVlJ9ZpV")
+      .set("spark.hadoop.fs.s3a.access.key", "nnICK7KOtzheuCh4aCW6")
+      .set("spark.hadoop.fs.s3a.secret.key", "g8lPsriEGfpJaym0PMUYcEwnKBp8g4GOGWg5UU8B")
       .set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
       .set("spark.hadoop.fs.s3a.path.style.access", "true")
       .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
-//      .set("hadoop.home.dir", "D:/application/hadoop")
-//      .set("HADOOP_HOME", "D:/application/hadoop")
-
       .setJars(Seq(
-        "D:\\code\\data_algorithms\\target\\scala-2.13\\data_algorithms_2.13-1.0.jar",
+        "/Users/adan/code/egg/data_algorithms/target/scala-2.13/data_algorithms_2.13-1.0.jar",
       ))
+  }
+
+  private def isMac: Boolean = {
+    System.getProperty("os.name").contains("Mac")
   }
 
 }
