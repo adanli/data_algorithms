d891d2be10df61262d41b9f65313c53579d76061
li adan <adanli@126.com>
2023-10-09 17:17:52 +0800

    feat(数据算法): init

--- src/main/scala/demo/SparkExample.scala
+++ src/main/scala/demo/SparkExample.scala
@@ -7,10 +7,26 @@ object SparkExample {
   def main(args: Array[String]): Unit = {
     val spark = SparkConfig.spark("spark-example")
 
-    val strings = spark.read.textFile("s3a://books/README.md")
+    /*val strings = spark.read.textFile("s3a://books/README.md")
     strings.show(10, truncate = false)
 
-    println(strings.filter(col("value").contains("Spark")).count())
+    println(strings.filter(col("value").contains("Spark")).count())*/
+
+
+    val claim = spark.read.option("header", "true").csv("s3a://data/系统清单数据/案件清单-20231008.csv")
+    claim.show(false)
+
+    val s1 = System.currentTimeMillis()
+    println(claim.count())
+    println(s"案件量统计耗时: ${System.currentTimeMillis() - s1}")
+
+    val claimByPolicy = spark.read.options(Map("header" -> "true")).csv("s3a://data/系统清单数据/赔案清单-20231008.csv")
+    claimByPolicy.show(false)
+
+    val s2 = System.currentTimeMillis()
+    println(claimByPolicy.count())
+    println(s"赔案量统计耗时: ${System.currentTimeMillis() - s2}")
+
   }
 
 }
